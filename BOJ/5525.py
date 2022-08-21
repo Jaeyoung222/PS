6f53a78c4bef15bba52db06e33cb7bb4c538616c
Devtist <yt930310@snu.ac.kr>
2022-08-21 23:18:33 +0900

    Add BOJ Directory

---
+++ BOJ/5525.py
@@ -0,0 +1,18 @@
+import sys
+
+
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+target = "I"+("OI"*n)
+
+m = int(input())
+
+s = input()
+c = 0
+for x in range(len(s)-(2*n+1)) :
+    if s[x:(x+(2*n)+1)] == target :
+        c += 1
+
+print(c)
